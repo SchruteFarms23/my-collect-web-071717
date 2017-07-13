a5babdb398996ab80628b30ae2152e7b6f2bd3de
SchruteFarms23 <romanmullady@gmail.com>
2017-07-12 23:07:43 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(languages)
+	i = 0
+	new_collection = []
+	while i < languages.length
+		new_element = yield(languages[i])
+		new_collection << new_element
+		i += 1
+	end
+	new_collection
+end
+
 
 
